b0fe59850016565ec6adc0fa00f2ada8bd3b7514
Koppa Dániel <100847378+KoDan76@users.noreply.github.com>
2023-05-24 21:10:34 +0200

    Update LoadTest.java

--- src/test/NonFunctionalTests/ReliabilityTests/LoadTest.java
+++ src/test/NonFunctionalTests/ReliabilityTests/LoadTest.java
@@ -8,51 +8,90 @@ import org.sonatype.guice.bean.containers.Main;
 
 import javax.swing.*;
 
+import java.awt.*;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LoadTest {
 
+    Thread checker = new Thread() {
+        volatile boolean escape = false;
+        @Override
+        public void run() {
+
+            while (!escape) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+                try {
+                    setUpTestEscape(firstStart, 2000);
+                    setUpTestEscape(secondStart, 2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+                escape = true;
+            }
+        }
+    };
+
     private class StartThread extends Thread {
-        boolean exit = true;
+        volatile boolean exit = false;
 
         public void setExit( ) {
             this.exit = true;
         }
 
+        MainMenu mainMenu;
+
+        public MainMenu getMainMenu () {
+            return mainMenu ;
+        }
         @Override
         public void run() {
             Game game = new Game();
             MainMenu mainMenu = new MainMenu(game);
-
+            System.out.println("Created");
             while(!exit){
 
             }
+            System.out.println("Closed");
         }
     }
-    public void setUpTestEscape(StartThread startThread) throws InterruptedException {
-        Thread.sleep(1000);
+    public void setUpTestEscape(StartThread startThread, int runTime) throws InterruptedException {
+        Thread.sleep(runTime);
         startThread.setExit();
     }
 
+    StartThread firstStart;
+    StartThread secondStart;
     @BeforeEach
     void SetUp() {
-        StartThread firstStart  = new StartThread();
-        StartThread secondStart  = new StartThread();
-        firstStart.start();
-        secondStart.start();
-
-        try {
-            setUpTestEscape(firstStart);
-            setUpTestEscape(secondStart);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        firstStart  = new StartThread();
+        secondStart  = new StartThread();
     }
 
     @Test
-    void main() {
+    void main() throws InterruptedException {
+        firstStart.start();
+        secondStart.start();
 
+        assertNull(secondStart.getMainMenu());
+
+        assertNotNull(firstStart.getMainMenu());
+        assertNull(secondStart.getMainMenu());
+        checker.start();
+        System.out.println("Checked");
+
+        try {
+            firstStart.join();
+            firstStart.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
