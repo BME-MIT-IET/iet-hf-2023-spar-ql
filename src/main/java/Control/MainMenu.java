ed4a0418357d751db31562a2bfaa0dc8352f4189
Koppa Dániel <100847378+KoDan76@users.noreply.github.com>
2023-05-24 20:01:04 +0200

    Reliability Test Added

--- src/main/java/Control/MainMenu.java
+++ src/main/java/Control/MainMenu.java
@@ -26,6 +26,7 @@ public class MainMenu {
 
     public MainMenu(Game game){
         this.game = game;
+
         init();
     }
 
--- src/test/NonFunctionalTests/ReliabilityTests/LoadTest.java
+++ src/test/NonFunctionalTests/ReliabilityTests/LoadTest.java
@@ -2,31 +2,57 @@ package NonFunctionalTests.ReliabilityTests;
 
 import Control.Game;
 import Control.MainMenu;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.sonatype.guice.bean.containers.Main;
+
+import javax.swing.*;
+
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LoadTest {
 
-    private class startThread extends Thread {
+    private class StartThread extends Thread {
+        boolean exit = true;
+
+        public void setExit( ) {
+            this.exit = true;
+        }
+
         @Override
         public void run() {
             Game game = new Game();
             MainMenu mainMenu = new MainMenu(game);
+
+            while(!exit){
+
+            }
+        }
+    }
+    public void setUpTestEscape(StartThread startThread) throws InterruptedException {
+        Thread.sleep(1000);
+        startThread.setExit();
+    }
+
+    @BeforeEach
+    void SetUp() {
+        StartThread firstStart  = new StartThread();
+        StartThread secondStart  = new StartThread();
+        firstStart.start();
+        secondStart.start();
+
+        try {
+            setUpTestEscape(firstStart);
+            setUpTestEscape(secondStart);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 
-
     @Test
     void main() {
-        Game game = new Game();
-        MainMenu mainMenu = new MainMenu(game);
-
-        //Thread firstStart  = new startThread();
-        //Thread secondStart  = new startThread();
-        //firstStart.start();
-        // secondStart.start();
 
     }
 }
