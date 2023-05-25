f27fb546b93d0d155ded9a74e4c339ff68f33b52
Koppa Dániel <100847378+KoDan76@users.noreply.github.com>
2023-05-25 13:06:31 +0200

    Create VirologistTest.java

---
+++ src/test/UnitTests/Model/VirologistTest.java
@@ -0,0 +1,189 @@
+package UnitTests.Model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VirologistTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void setBag() {
+    }
+
+    @Test
+    void setTile() {
+    }
+
+    @Test
+    void setMap() {
+    }
+
+    @Test
+    void setView() {
+    }
+
+    @Test
+    void move() {
+    }
+
+    @Test
+    void pointInPoly() {
+    }
+
+    @Test
+    void getMaxValue() {
+    }
+
+    @Test
+    void getMinValue() {
+    }
+
+    @Test
+    void getUntouchable() {
+    }
+
+    @Test
+    void getEffects() {
+    }
+
+    @Test
+    void useAgent() {
+    }
+
+    @Test
+    void testUseAgent() {
+    }
+
+    @Test
+    void takeGear() {
+    }
+
+    @Test
+    void learnCode() {
+    }
+
+    @Test
+    void collectProtectiveGear() {
+    }
+
+    @Test
+    void palpateWall() {
+    }
+
+    @Test
+    void wear() {
+    }
+
+    @Test
+    void hitByAgent() {
+    }
+
+    @Test
+    void giveGear() {
+    }
+
+    @Test
+    void collectMaterial() {
+    }
+
+    @Test
+    void addAgent() {
+    }
+
+    @Test
+    void getBag() {
+    }
+
+    @Test
+    void getWear() {
+    }
+
+    @Test
+    void unwear() {
+    }
+
+    @Test
+    void die() {
+    }
+
+    @Test
+    void useMaterials() {
+    }
+
+    @Test
+    void getTile() {
+    }
+
+    @Test
+    void addEffect() {
+    }
+
+    @Test
+    void vitusDanceActionPerform() {
+    }
+
+    @Test
+    void bearDanceActionPerform() {
+    }
+
+    @Test
+    void getGeneticCodes() {
+    }
+
+    @Test
+    void setCodeCount() {
+    }
+
+    @Test
+    void setAgentResistance() {
+    }
+
+    @Test
+    void setThrowBackAvailable() {
+    }
+
+    @Test
+    void getAgentResistance() {
+    }
+
+    @Test
+    void isThrowBackAvailable() {
+    }
+
+    @Test
+    void setUntouchbale() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getCodeCount() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void print() {
+    }
+
+    @Test
+    void removeEffect() {
+    }
+
+    @Test
+    void getBearDance() {
+    }
+
+    @Test
+    void getVitusDance() {
+    }
+}
